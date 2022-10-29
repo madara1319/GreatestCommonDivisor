e77619aa8abb86ab1fa09cdc03dae03e52ce1733
madara1319 <mateusz.bik@op.pl>
2022-10-29 19:24:27 +0200

    Kinda works

--- GCD.cpp
+++ GCD.cpp
@@ -3,8 +3,13 @@
 
 using std::cout;
 
+void NWD(){
+	cout<<"Hello world!"<<"\n";
+}
+
 int main(){
-//	cout << "Hello world";
+	NWD;
+	cout << "Program to znajdowania algorytmu Najwiekszego Wspolnego Dzielnika metoda Euklidesa"<<"\n";
 	int opt1=1;
 	int opt2=2;
 	int decision={};
@@ -13,29 +18,36 @@ int main(){
 	while(true)
 	{
 		if (decision==opt1){
-			int a=250;
-			int b=45;
+			int a={};
+			int b={};
+			cout << "Wprowadz pierwsza liczbe"<<"\n";
+			std::cin >> a;
+			cout << "Wprowadz druga liczbe"<<"\n";
+			std::cin >> b;
 			while(b!=0){
-				cout <<"Wartosc a = "<<a<<"\n";
-				cout <<"Wartosc b = "<<b<<"\n";	
+			//	cout <<"Wartosc a = "<<a<<"\n";
+			//	cout <<"Wartosc b = "<<b<<"\n";	
 				int tmp=b;
 				b=a%b;
 				a=tmp;
 				
-				cout <<"Wartosc a = "<<a<<"\n";
-				cout <<"Wartosc b = "<<b<<"\n";
-				return a;
+			//	cout <<"Wartosc a = "<<a<<"\n";
+
+				
+			//	return a;
 			}
+			cout <<"Najwiekszy wspolny dzielnik wynosi GC(a,b) = "<<a<<"\n";
+		break;	
+		}
 		if (decision==opt2){
 			cout<<"Wybrales zakonczenie programu";
-		
+		return false;
 		}
-		else
+		if (decision=!opt1 || decision!=opt2)	
 		{
 			cout<<"Niepoprawna opcja"<<"\n";
+			continue;
 		}
-	break;
-	}
 	}
 }
 
