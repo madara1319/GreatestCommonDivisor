d23f1571086b64aaceca26a6972c4358a32f8094
madara1319 <mateusz.bik@op.pl>
2022-10-30 13:42:29 +0100

    Still functions dont work the way id like to

--- GCD.cpp
+++ GCD.cpp
@@ -3,14 +3,45 @@
 
 using std::cout;
 
-int NWD(){
+int hello(){
 	cout<<"Hello world!"<<"\n";
 	return 0;
 }
 
+int GCD()
+{
+
+	int a={};
+	int b={};
+	cout << "Write first number"<<"\n";
+	std::cin >> a;
+	cout << "Write second number"<<"\n";
+	std::cin >> b;
+	while(b!=0){
+		int tmp=b;
+		b=a%b;
+		a=tmp;
+	}
+	cout <<"Greatest common Divisor equals GCD(a,b) = "<<a<<"\n";
+	return 0;
+	
+}
+int menu()
+{
+	int test;
+	cout << "This simple app is to find the greatest common divisor using Euclidean method"<<"\n";
+	int opt1=1;
+	int opt2=2;
+	int decision={};
+	cout <<"To start app write 1 to exit write 2"<<"\n";
+	std::cin >> decision;
+	return 0;
+
+}
+
 int main(){
 	int test;
-	test=NWD();
+	int test2;
 	cout << "This simple app is to find the greatest common divisor using Euclidean method"<<"\n";
 	int opt1=1;
 	int opt2=2;
@@ -20,25 +51,7 @@ int main(){
 	while(true)
 	{
 		if (decision==opt1){
-			int a={};
-			int b={};
-			cout << "Write first number"<<"\n";
-			std::cin >> a;
-			cout << "Write second number"<<"\n";
-			std::cin >> b;
-			while(b!=0){
-			//	cout <<"Wartosc a = "<<a<<"\n";
-			//	cout <<"Wartosc b = "<<b<<"\n";	
-				int tmp=b;
-				b=a%b;
-				a=tmp;
-				
-			//	cout <<"Wartosc a = "<<a<<"\n";
-
-				
-			//	return a;
-			}
-			cout <<"Greatest common Divisor equals GCD(a,b) = "<<a<<"\n";
+		test=GCD();
 		break;	
 		}
 		if (decision==opt2){
@@ -48,8 +61,12 @@ int main(){
 		if (decision=!opt1 || decision!=opt2)	
 		{
 			cout<<"Incorrect option"<<"\n";
-		return false;
+			test2=menu(); 	
+			test=GCD();
+		
+
 		}
+
 	}
 }
 
