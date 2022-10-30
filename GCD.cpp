7b0f64fcfecf86fb0fc6a155df16f46e09d28aa1
madara1319 <mateusz.bik@op.pl>
2022-10-30 13:25:00 +0100

    Testing some about functions

--- GCD.cpp
+++ GCD.cpp
@@ -3,26 +3,28 @@
 
 using std::cout;
 
-void NWD(){
+int NWD(){
 	cout<<"Hello world!"<<"\n";
+	return 0;
 }
 
 int main(){
-	NWD;
-	cout << "Program to znajdowania algorytmu Najwiekszego Wspolnego Dzielnika metoda Euklidesa"<<"\n";
+	int test;
+	test=NWD();
+	cout << "This simple app is to find the greatest common divisor using Euclidean method"<<"\n";
 	int opt1=1;
 	int opt2=2;
 	int decision={};
-	cout <<"Jesli chcesz znalezc najwiekszy wspolny dzielnik 2 liczb wpisz 1 lub jesli chcesz zakonczyc dzialanie programu wpisz 2"<<"\n";
+	cout <<"To start app write 1 to exit write 2"<<"\n";
 	std::cin >> decision;
 	while(true)
 	{
 		if (decision==opt1){
 			int a={};
 			int b={};
-			cout << "Wprowadz pierwsza liczbe"<<"\n";
+			cout << "Write first number"<<"\n";
 			std::cin >> a;
-			cout << "Wprowadz druga liczbe"<<"\n";
+			cout << "Write second number"<<"\n";
 			std::cin >> b;
 			while(b!=0){
 			//	cout <<"Wartosc a = "<<a<<"\n";
@@ -36,17 +38,17 @@ int main(){
 				
 			//	return a;
 			}
-			cout <<"Najwiekszy wspolny dzielnik wynosi GC(a,b) = "<<a<<"\n";
+			cout <<"Greatest common Divisor equals GCD(a,b) = "<<a<<"\n";
 		break;	
 		}
 		if (decision==opt2){
-			cout<<"Wybrales zakonczenie programu";
+			cout<<"You chose to exit the app";
 		return false;
 		}
 		if (decision=!opt1 || decision!=opt2)	
 		{
-			cout<<"Niepoprawna opcja"<<"\n";
-			continue;
+			cout<<"Incorrect option"<<"\n";
+		return false;
 		}
 	}
 }
